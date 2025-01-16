d9d414609776146a113ff6221ed7bf527c351961
clarklindev <clarklindev@gmail.com>
2025-01-17 07:59:36 +0800

    updated master - STRIPE_KEY secret added to github actions payments workflow

--- payments/src/index.ts
+++ payments/src/index.ts
@@ -9,6 +9,8 @@ import { OrderCreatedListener } from './events/listeners/order-created-listener'
 
 
 const start = async () => {
+  console.log('starting');
+  
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
